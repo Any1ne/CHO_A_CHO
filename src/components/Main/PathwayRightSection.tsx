a8bb94dc949a438fc350dbdd18a58e086382b183
Any1ne <anytguy@gmail.com>
2025-06-29 19:53:40 +0300

    Update PathwayRightSection.tsx

--- src/components/Main/PathwayRightSection.tsx
+++ src/components/Main/PathwayRightSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Autoplay from "embla-carousel-autoplay";
 import {
