d9e9ac3e25c90688212170eff4c217d237ab3d6e
Anyone <anytguy@gmail.com>
2025-07-01 02:23:03 +0300

    Open Graph settings

---
+++ src/config/next-seo.config.ts
@@ -0,0 +1,26 @@
+const defaultSEO = {
+  title: "CHO A CHO Store",
+  description: "Online store of CHO A CHO brand of chocolate",
+  canonical: "https://www.choacho.com.ua",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.choacho.com.ua",
+    site_name: "CHO A CHO Store",
+    images: [
+      {
+        url: "https://www.choacho.com.ua/og-image.jpg",
+        width: 1056,
+        height: 1056,
+        alt: "CHO A CHO Store",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@choacho",
+    site: "@choacho",
+    cardType: "summary_large_image",
+  },
+};
+
+export default defaultSEO;
